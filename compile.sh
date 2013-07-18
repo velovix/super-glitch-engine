e5f26cf354ebc8e79e33478a62cc05352de9c6f6
Tyler Compton <xaviosx@gmail.com>
2013-07-17 19:55:44 -0700

    Update compile shell script.

--- compile.sh
+++ compile.sh
@@ -2,6 +2,12 @@ echo Compiling SGE client and tools...
 echo --- Compiling Type Maker
 cd TypeMaker
 make
+echo --- Compiling Move Maker
+cd ../MoveMaker
+make
+echo --- Compiling Map Maker
+cd ../MapMaker
+make
 echo --- Compiling Client
 cd ../Client
 mkdir out
