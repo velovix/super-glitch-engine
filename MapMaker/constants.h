279dd8bb549e35a926f7128fe798bca2ba418173
Velovix <xaviosx@gmail.com>
2013-09-08 16:51:22 -0700

    Get rid of old constants and files.

--- MapMaker/constants.h
+++ MapMaker/constants.h
@@ -1,40 +1,6 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define GRASS			0
-#define ONE_POLL		1
-#define DARK_DIRT		2
-#define TREE			3
-#define SIGN			4
-#define WATER			5
-#define R_SHORE			6
-#define L_SHORE			7
-#define U_SHORE			8
-#define U_R_SHORE		9
-#define U_L_SHORE		10
-#define TWO_POLL		11
 #define CLEAR			12
-#define GRASS_OVER		13
-#define WORD_BUBBLE		14
-#define HOUSE_LROOF		15
-#define HOUSE_MROOF		16
-#define HOUSE_RTOFLOORL	17
-#define HOUSE_DOOR		18
-#define HOUSE_RWIND		19
-#define HOUSE_LWIND		20
-#define HOUSE_DWIND		21
-#define HOUSE_RTOFLOORR	22
-#define HOUSE_RROOF		23
-#define LIGHT_DIRT		24
-#define CLIFF_ENTRY		25
-#define CLIFF_LCORNER	26
-#define CLIFF_MIDDLE	27
-
-#define DOOR_PAD		201
-
-#define MAX_WIDTH		128
-#define MAX_HEIGHT		128
-#define BLOCK_SIZE		16
-#define MAX_ROOMS		20
 
 #endif
