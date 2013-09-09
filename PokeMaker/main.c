7ac1c32b1bfc6dc2ebf2c9c2bef8025c69fe025e
Velovix <xaviosx@gmail.com>
2013-09-08 23:49:09 -0700

    Implement preliminary functionality to Pokemon Maker (WIP).

--- PokeMaker/main.c
+++ PokeMaker/main.c
@@ -3,7 +3,9 @@
 #include <stdbool.h>
 #include <gtk/gtk.h>
 
-#include "headers.h"
+#include "../common/typeFile.h"
+#include "../common/moveFile.h"
+#include "../common/monFile.h"
 
 // Window constants
 #define W_MAIN 			0
@@ -14,102 +16,201 @@
 #define W_EVOLUTIONS	5
 #define W_EXP			6
 
-GtkWidget *windows[5];
+int currMon = -1;
+
+GtkWidget *windows[7];
 int currEditWindow = 0;
 
 // Widgets
-GtkWidget *tv_monsters;
+GtkWidget *cb_evolves;
+GtkWidget *cb_nogender;
+GtkWidget *sb_genderratio;
+GtkWidget *sb_evolevel;
+GtkWidget *e_name;
 
-void toggleEditable(gboolean editable)
-{
+GtkWidget *cbt_monsters;
+GtkWidget *cbt_evomon;
+GtkWidget *cbt_type1, *cbt_type2;
+
+GtkWidget *sb_hp, *sb_att, *sb_def, *sb_spatt, *sb_spdef, *sb_speed;
+GtkWidget *sb_ehp, *sb_eatt, *sp_edef, *sp_espatt, *sp_espdef, *sp_espeed;
+
+monFile_t monFile;
+
+void toggleEditable(gboolean editable) {
 
 }
 
+void newMonFile() {
+	// Prime the monFile_t for later use
+	monFile.header.version = monFILE_VERSION;
+	monFile.header.count = 0;
+
+	monFile.mons = NULL;
+}
+
+void newMon(char *name) {
+	// Allocate more monster data
+	monFile.mons =
+		(monFileObj_t*)realloc(monFile.mons, sizeof(monFileObj_t)*(monFile.header.count+1));
+
+	int curr = monFile.header.count;
+	// Set the name
+	for(int i=0; i<10; i++) {
+		monFile.mons[curr].basic.name[i] = name[i];
+	}
+
+	// Make all settings default
+	monFile.mons[curr].basic.type1 = monFile.mons[curr].basic.type2 = 0;
+	monFile.mons[curr].basic.height = monFile.mons[curr].basic.weight = 0;
+	monFile.mons[curr].stats.b_hp = monFile.mons[curr].stats.b_att =
+		monFile.mons[curr].stats.b_def = 0;
+	monFile.mons[curr].stats.b_spAtt = monFile.mons[curr].stats.b_spDef =
+		monFile.mons[curr].stats.b_speed = 0;
+	monFile.mons[curr].stats.ev_hp = monFile.mons[curr].stats.ev_att =
+		monFile.mons[curr].stats.ev_def = 0;
+	monFile.mons[curr].stats.ev_spAtt = monFile.mons[curr].stats.ev_spDef =
+		monFile.mons[curr].stats.ev_speed = 0;
+	monFile.mons[curr].movelist.natLearnedCnt = monFile.mons[curr].movelist.tmLearnedCnt = 0;
+	monFile.mons[curr].movelist.natLearned = NULL;
+	monFile.mons[curr].movelist.tmLearned = NULL;
+	monFile.mons[curr].experience.type = 0;
+	monFile.mons[curr].evolution.evolves = 0;
+	monFile.mons[curr].aesthetics.battleSprite = monFile.mons[curr].aesthetics.icon =
+		monFile.mons[curr].aesthetics.body = monFile.mons[curr].aesthetics.footprint = 0;
+	monFile.mons[curr].etc.genderless = 0;
+	monFile.mons[curr].etc.malePerc = 50;
+	monFile.mons[curr].etc.ability = 0;
+	monFile.mons[curr].etc.catchRate = 255;
+	monFile.mons[curr].etc.heldItem = 0;
+
+	monFile.header.count++;
+}
+
+void updateMons() {
+	// Remove the combo box entries and reinput to guarantee order
+	gtk_combo_box_text_remove_all(GTK_COMBO_BOX_TEXT(cbt_monsters));
+	gtk_combo_box_text_remove_all(GTK_COMBO_BOX_TEXT(cbt_evomon));
+	for(int i=0; i<monFile.header.count; i++) {
+		gtk_combo_box_text_insert_text(GTK_COMBO_BOX_TEXT(cbt_monsters), i,
+			monFile.mons[i].basic.name);
+		gtk_combo_box_text_insert_text(GTK_COMBO_BOX_TEXT(cbt_evomon), i,
+			monFile.mons[i].basic.name);
+	}
+}
+
 void openEditWindow(int newWindow) {
-	//gtk_widget_hide(windows[currWindow]);
 	gtk_widget_show(windows[newWindow]);
 }
 
 // --- MAIN WINDOW BUTTONS CLICKED ---
-void b_editstats_clicked(GtkWidget *obj, gpointer user_data)
-{
+void b_editstats_clicked(GtkWidget *obj, gpointer user_data) {
 	openEditWindow(W_STATS);
 }
 
-void b_editaesthetics_clicked(GtkWidget *obj, gpointer user_data)
-{
+void b_editaesthetics_clicked(GtkWidget *obj, gpointer user_data) {
 	openEditWindow(W_AESTHETICS);
 }
 
-void b_editmovelist_clicked(GtkWidget *obj, gpointer user_data)
-{
+void b_editmovelist_clicked(GtkWidget *obj, gpointer user_data) {
 	openEditWindow(W_MOVES);
 }
 
-void b_editothers_clicked(GtkWidget *obj, gpointer user_data)
-{
+void b_editothers_clicked(GtkWidget *obj, gpointer user_data) {
 	openEditWindow(W_OTHERS);
 }
 
-void b_editevolutions_clicked(GtkWidget *obj, gpointer user_data)
-{
+void b_editevolutions_clicked(GtkWidget *obj, gpointer user_data) {
 	openEditWindow(W_EVOLUTIONS);
 }
 
-void b_editexp_clicked(GtkWidget *obj, gpointer user_data)
-{
+void b_editexp_clicked(GtkWidget *obj, gpointer user_data) {
 	openEditWindow(W_EXP);
 }
 
 // --- OTHER STUFF
-void b_new_clicked(GtkWidget *obj, gpointer user_data)
-{
-	GtkTreeIter iter;
-	GtkTreeModel *model = gtk_tree_view_get_model(GTK_TREE_VIEW(tv_monsters));
-	gtk_list_store_append(GTK_LIST_STORE(model), &iter);
-	gtk_list_store_set(GTK_LIST_STORE(model), &iter, 0, "NEW", -1);
+void cb_evolves_toggled(GtkWidget *obj, gpointer user_data) {
+	gboolean on = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(obj));
+
+	gtk_widget_set_sensitive(cbt_evomon, on);
+	gtk_widget_set_sensitive(sb_evolevel, on);
 }
 
-void loadTypes(char *filename)
-{
-	printf("Loading types..\n");
-	FILE * file;
-	file = fopen(filename, "r");
+void cb_nogender_toggled(GtkWidget *obj, gpointer user_data) {
+	gboolean on = !gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(obj));
 
-	if(file == NULL) {
-		printf("[ERROR] Missing types.pke file!\n");
+	gtk_widget_set_sensitive(sb_genderratio, on);
+}
+
+void b_new_clicked(GtkWidget *obj, gpointer user_data) {
+	newMon("NEW");
+
+	updateMons();
+
+	// Select the newly made monster
+	gtk_combo_box_set_active(GTK_COMBO_BOX(cbt_monsters), monFile.header.count-1);
+}
+
+void cbt_monsters_changed(GtkWidget *obj, gpointer user_data) {
+
+	currMon = gtk_combo_box_get_active(GTK_COMBO_BOX(obj));
+
+	// If no monster is selected, nothing else needs to be done
+	if(currMon == -1) {
 		return;
 	}
 
-	typeHeader_f header;
-	fread(&header.version, sizeof(int), 1, file);
-	if(header.version != TYPE_VERSION) {
-		printf("[ERROR] types.pke is V%i, but V%i is expected!\n",
-			header.version, TYPE_VERSION);
-		return;
-	}
-	fread(&header.count, sizeof(int), 1, file);
-	
-	for(int i=0; i<header.count; i++) {
-		char tmpChar[8];
-		int resCnt, weakCnt;
-		fread(&tmpChar[0], sizeof(char[8]), 1, file);
-		fread(&resCnt, sizeof(int), 1, file);
-		fread(&weakCnt, sizeof(int), 1, file);
-
-		printf("   %s\n", tmpChar);
-
-		fseek(file, resCnt*(sizeof(int)+sizeof(char))+weakCnt*sizeof(int),
-			SEEK_CUR);
-	}
-
-	fclose(file);
-
-	printf("done!\n");
+	// Update widgets to reflect monster
+	gtk_entry_set_text(GTK_ENTRY(e_name), gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(obj)));
 }
 
-void on_window_destroy (GtkWidget *object, gpointer user_data)
-{
+void b_update_clicked(GtkWidget *obj, gpointer user_data) {
+
+	// GTK likes to reset dropdowns when they are changed
+	int tmpCurrMon = currMon;
+
+	char *name = (char*)gtk_entry_get_text(GTK_ENTRY(e_name));
+
+	bool fill = false;
+	for(int i=0; i<8; i++) {
+		if(name[i] == ' ' && name[i+1] == ' ') {
+			fill = true;
+		}
+		if(fill) {
+			monFile.mons[currMon].basic.name[i] = ' ';
+		} else {
+			monFile.mons[currMon].basic.name[i] = name[i];
+		}
+	}
+
+	updateMons();
+
+	// Set the combo boxes back to their original state
+	gtk_combo_box_set_active(GTK_COMBO_BOX(cbt_monsters), tmpCurrMon);
+}
+
+void loadTypes(char *filename) {
+	typeFile_t typeFile;
+
+	pk_openTypeFile(&typeFile, filename);
+
+	for(int i=0; i<typeFile.header.count; i++) {
+		gtk_combo_box_text_insert_text(GTK_COMBO_BOX_TEXT(cbt_type1), i, typeFile.types[i].header.name);
+		gtk_combo_box_text_insert_text(GTK_COMBO_BOX_TEXT(cbt_type2), i, typeFile.types[i].header.name);
+	}
+
+	pk_freeTypeFile(&typeFile);
+}
+
+void loadMoves(char *filename) {
+	moveFile_t moveFile;
+
+	pk_openMoveFile(&moveFile, filename);
+
+	pk_freeMoveFile(&moveFile);
+}
+
+void on_window_destroy (GtkWidget *object, gpointer user_data) {
 	gtk_main_quit();
 }
 
@@ -131,28 +232,42 @@ int main(int argc, char **argv) {
 	windows[W_EXP] =		GTK_WIDGET (gtk_builder_get_object (builder, "w_exp"));
 	windows[W_EVOLUTIONS] =	GTK_WIDGET (gtk_builder_get_object (builder, "w_evolutions"));
 
-	// Load up individual objects
-	tv_monsters = GTK_WIDGET(gtk_builder_get_object(builder, "tv_monsters"));
+	// Make extra windows only hide when you "x" out
+	for(int i=W_MOVES; i<7; i++) {
+		g_signal_connect (G_OBJECT(windows[i]), "delete-event", G_CALLBACK(gtk_widget_hide_on_delete), NULL);
+	}
+
+	// Load widgets
+	cb_evolves =			GTK_WIDGET (gtk_builder_get_object(builder, "cb_evolves"));
+	cb_nogender = 			GTK_WIDGET (gtk_builder_get_object(builder, "cb_nogender"));
+	sb_genderratio = 		GTK_WIDGET (gtk_builder_get_object(builder,"sb_genderratio"));
+	sb_evolevel = 			GTK_WIDGET (gtk_builder_get_object(builder, "sb_evolevel"));
+	e_name = 				GTK_WIDGET (gtk_builder_get_object(builder, "e_name"));
+
+	cbt_monsters =			GTK_WIDGET (gtk_builder_get_object(builder, "cbt_monsters"));
+	cbt_evomon = 			GTK_WIDGET (gtk_builder_get_object(builder, "cbt_evomon"));
+	cbt_type1 = 			GTK_WIDGET (gtk_builder_get_object(builder, "cbt_type1"));
+	cbt_type2 = 			GTK_WIDGET (gtk_builder_get_object(builder, "cbt_type2"));
+
+	sb_hp = 				GTK_WIDGET (gtk_builder_get_object(builder, "sb_hp"));
+	sb_att = 				GTK_WIDGET (gtk_builder_get_object(builder, "cb_att"));
+	sb_def = 				GTK_WIDGET (gtk_builder_get_object(builder, "cb_def"));
+	sb_spatt = 				GTK_WIDGET (gtk_builder_get_object(builder, "cb_spatt"));
+	sb_spdef = 				GTK_WIDGET (gtk_builder_get_object(builder, "cb_spdef"));
+	sb_speed = 				GTK_WIDGET (gtk_builder_get_object(builder, "cb_speed"));
+	sb_ehp = 				GTK_WIDGET (gtk_builder_get_object(builder, "cb_ehp"));
+	sb_eatt = 				GTK_WIDGET (gtk_builder_get_object(builder, "cb_eatt"));
+	sp_edef = 				GTK_WIDGET (gtk_builder_get_object(builder, "cb_edef"));
+	sp_espatt = 			GTK_WIDGET (gtk_builder_get_object(builder, "cb_espatt"));
+	sp_espdef = 			GTK_WIDGET (gtk_builder_get_object(builder, "cb_espdef"));
+	sp_espeed = 			GTK_WIDGET (gtk_builder_get_object(builder, "cb_espeed"));
 
 	gtk_builder_connect_signals(builder, NULL);
 	g_object_unref(G_OBJECT(builder));
 
 	// Load external resources
 	loadTypes("../resources/data/types.pke");
-
-
-	// Set up the Tree View. Would be done in Glade, but I can't get it working
-	// Set up data structure
-	GtkTreeIter iter;
-	GtkListStore *ls_tv_monsters = gtk_list_store_new(1, G_TYPE_STRING);
-	gtk_tree_view_set_model(GTK_TREE_VIEW(tv_monsters), GTK_TREE_MODEL(ls_tv_monsters));
-	gtk_list_store_append(ls_tv_monsters, &iter);
-	gtk_list_store_set(ls_tv_monsters, &iter, 0, "NEW", -1);
-	// Set up display for Tree View
-	GtkCellRenderer *renderer = gtk_cell_renderer_text_new();
-	GtkTreeViewColumn *col = gtk_tree_view_column_new();
-	col = gtk_tree_view_column_new_with_attributes ("Monster", renderer, "text", 0, NULL);
-	gtk_tree_view_append_column(GTK_TREE_VIEW(tv_monsters), col);
+	loadMoves("../resources/data/moves.pke");
 
 	toggleEditable(FALSE);
 
---
+++ common/monFile.h
@@ -0,0 +1,100 @@
+#ifndef monFILE_H
+#define monFILE_H
+
+#include <stdint.h>
+
+#define monFILE_VERSION		1
+
+/* -- Header -- */
+typedef struct {
+	uint32_t version;
+	uint32_t count;
+} monHeader_f;
+
+/* -- Basics -- */
+typedef struct {
+	uint8_t name[10];
+	uint8_t type1;
+	uint8_t type2;
+	uint32_t height;
+	uint32_t weight;
+} monBasic_f;
+
+/* -- Stats -- */
+typedef struct {
+	uint8_t b_hp;
+	uint8_t b_att;
+	uint8_t b_def;
+	uint8_t b_spAtt;
+	uint8_t b_spDef;
+	uint8_t b_speed;
+
+	uint8_t ev_hp;
+	uint8_t ev_att;
+	uint8_t ev_def;
+	uint8_t ev_spAtt;
+	uint8_t ev_spDef;
+	uint8_t ev_speed;
+} monStats_f;
+
+/* -- Move List -- */
+typedef struct {
+	uint32_t move;
+	uint8_t level;
+} monNatMoveEntry_f;
+
+typedef struct {
+	uint32_t natLearnedCnt;
+	monNatMoveEntry_f *natLearned;
+
+	uint32_t tmLearnedCnt;
+	uint32_t *tmLearned;
+} monMovelist_f;
+
+/* -- Experience -- */
+typedef struct {
+	uint8_t type;
+} monExperience_f;
+
+/* -- Evolutions -- */
+typedef struct {
+	uint8_t evolves;
+	uint32_t mon;
+	uint8_t level;
+} monEvolution_f;
+
+/* -- Aesthetics -- */
+typedef struct {
+	uint32_t battleSprite;
+	uint32_t icon;
+	uint32_t body;
+	uint32_t footprint;
+} monAesthetics_f;
+
+/* -- Others -- */
+typedef struct {
+	uint8_t species[13];
+	uint8_t genderless;
+	uint8_t malePerc;
+	uint32_t ability;
+	uint8_t catchRate;
+	uint32_t heldItem;
+} monOthers_f;
+
+/* -- File Objects -- */
+typedef struct {
+	monBasic_f basic;
+	monStats_f stats;
+	monMovelist_f movelist;
+	monExperience_f experience;
+	monEvolution_f evolution;
+	monAesthetics_f aesthetics;
+	monOthers_f etc;
+} monFileObj_t;
+
+typedef struct {
+	monHeader_f header;
+	monFileObj_t *mons;
+} monFile_t;
+
+#endif
